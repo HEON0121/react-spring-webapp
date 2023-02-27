c48009588f6bbec5ec40a07042f5a614b88e6eaa
seheon Kim <seheon.emma@gmail.com>
2023-02-27 10:16:33 +1000

    feat. Webapp basic design

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import './App.css';
-import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
-
+import axios from 'axios';
+import Layout from './components/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
 function App() {
   
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
 
+    
   const getMovies = async () => {
-    
     try {
-      const response = await api.get("/api/v1/movies")
-      console.log(response.data)
-      setMovies(response.data);  
-    } catch(e) {
-      console.log(e);
+      const response = await axios.get('/api/v1/movies');
+      setMovies(response.data);
+    } catch (err){
+      console.log("err >>", err);
     }
-    
-    
+                       
     
   }
-  useEffect(()=>{
-    getMovies();
-  })
-  return (
+      useEffect(() => {
+        getMovies();
+       },[]);
+    
+    
+    
+    
+  
+  return ( 
+    
     <div className="App">
-
+      <Header/>      
+        <Routes>
+          <Route path='/' element={<Layout/>}>
+            <Route path='/' element={<Home movies={movies}/>}></Route>
+          </Route>
+      </Routes>
+      
+      
     </div>
   );
 }
